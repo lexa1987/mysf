45d7ba28c312fac25ac4bd0672ea5e5805d1ca1c
Alexey Burtsev <a.burcev@roseltorg.ru>
2017-12-01 17:16:47 +1000

    Chapter 7: Twig: For a Good time with Templates

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -14,9 +14,16 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
+
         $templating = $this->container->get('templating');
         return $this->render('genus/show.html.twig', [
-            'name' => $genusName
+            'name' => $genusName,
+            'notes' => $notes
         ]);
     }
 }
